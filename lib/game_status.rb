111924361d08e16ecf676cdaf8f66d8cd31e25af
Joana Palero <joana.palero24@gmail.com>
2018-04-27 00:35:30 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -42,7 +42,7 @@ end
 
 # full
 def full?(board)
-  return board.all?{|space| space == "X" || space == "O"}
+  return board.all?{|combination| combination == "X" || combination == "O"}
 end
 
 # draw
